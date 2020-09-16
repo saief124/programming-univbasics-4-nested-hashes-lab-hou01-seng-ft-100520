2e3578ac2e071bb956b4c6cfe4b7e4e31233ede4
Saief I Sayed <saiefsayed123@hotmail.com>
2020-09-16 04:03:16 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -60,11 +60,11 @@ end
 
 def adding_matz
 # add the following information to the top level of programmer_hash
-programmer_hash[:yukihiro_matsumoto => {
+programmer_hash[:yukihiro_matsumoto] => {
    :known_for => "Ruby",
    :languages => ["LISP", "C"]
  }
-]# return the entire updated hash
+# return the entire updated hash
 #
 
 
